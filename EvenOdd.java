d2a7222d2e6835ddf1e9f5f732d7ed51056def4d
Ajay Gupta <148214882+ajaygupta8434@users.noreply.github.com>
2025-09-26 15:23:48 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int i = 1; 
+
+        do {
+            if(i%2 == 0 ){
+            System.out.println("Even number :"+i);
+}
+
+            i++; 
+        } while (i <= 20);
+    }
+}
